3423fb2aa3f8e926f08980b230018dd71b563edb
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-02-27 15:41:09 +0100

    Handle non-integer inputs for salary

--- StaffRegister/Program.cs
+++ StaffRegister/Program.cs
@@ -9,7 +9,7 @@ while (!done)
     Console.Clear();
     Console.WriteLine("Välj 1 för att mata in en ny anställd.");
     Console.WriteLine("Välj 2 för att se en lista.");
-    Console.WriteLine("Välj 3 för att avsluta");
+    Console.WriteLine("Välj 3 för att avsluta.");
     string input = Console.ReadLine();
     if (input == "1")
     {
@@ -19,7 +19,7 @@ while (!done)
     {
         Console.Clear();
         Console.WriteLine(list.TextRepresentation());
-        Console.WriteLine("Tryck Enter för att komma tillbaka till menyn");
+        Console.WriteLine("Tryck Enter för att komma tillbaka till menyn.");
         Console.ReadLine();
     }
     else if(input == "3")
@@ -29,7 +29,7 @@ while (!done)
     else
     {
         Console.WriteLine("{0} är inte ett känt kommando", input);
-        Console.WriteLine("Tryck Enter för att komma tillbaka till menyn");
+        Console.WriteLine("Tryck Enter för att komma tillbaka till menyn.");
         Console.ReadLine();
     }
 }
@@ -40,9 +40,18 @@ static void AddEmployee(EmployeeList list)
     Console.WriteLine("Ny anställd");
     Console.Write("Ange namn: ");
     string name = Console.ReadLine();
-    Console.Write("Ange lön: ");
-    string salaryString = Console.ReadLine();
-    int salary = int.Parse(salaryString);
+    bool done = false;
+    int salary = 0;
+    while (!done)
+    {
+        Console.Write("Ange lön: ");
+        string salaryString = Console.ReadLine();
+        done = int.TryParse(salaryString, out salary);
+        if (!done)
+        {
+            Console.WriteLine("{0} är inte en giltig lön.", salaryString);
+        }
+    }
     list.AddEmployee(new Employee(name, salary));
 
 }
