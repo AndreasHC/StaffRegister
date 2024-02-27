7bff9262f3937b56f6c68e6f1474cfcd687e7cad
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-02-27 15:27:22 +0100

    User interface.

--- StaffRegister/Program.cs
+++ StaffRegister/Program.cs
@@ -1,5 +1,48 @@
 ﻿using StaffRegister;
 
 EmployeeList list = new EmployeeList();
-list.AddEmployee( new Employee("me", 2000));
-Console.WriteLine(list.TextRepresentation());
+
+bool done = false;
+
+while (!done)
+{
+    Console.Clear();
+    Console.WriteLine("Välj 1 för att mata in en ny anställd.");
+    Console.WriteLine("Välj 2 för att se en lista.");
+    Console.WriteLine("Välj 3 för att avsluta");
+    string input = Console.ReadLine();
+    if (input == "1")
+    {
+        AddEmployee(list);
+    }
+    else if (input == "2")
+    {
+        Console.Clear();
+        Console.WriteLine(list.TextRepresentation());
+        Console.WriteLine("Tryck Enter för att komma tillbaka till menyn");
+        Console.ReadLine();
+    }
+    else if(input == "3")
+    {
+        done = true;
+    }
+    else
+    {
+        Console.WriteLine("{0} är inte ett känt kommando", input);
+        Console.WriteLine("Tryck Enter för att komma tillbaka till menyn");
+        Console.ReadLine();
+    }
+}
+
+static void AddEmployee(EmployeeList list)
+{
+    Console.Clear();
+    Console.WriteLine("Ny anställd");
+    Console.Write("Ange namn: ");
+    string name = Console.ReadLine();
+    Console.Write("Ange lön: ");
+    string salaryString = Console.ReadLine();
+    int salary = int.Parse(salaryString);
+    list.AddEmployee(new Employee(name, salary));
+
+}
